bca32a9f871cdaa12cd106ee7362996df45691bd
MRG <aommiez@gmail.com>
2016-11-16 05:56:24 +0700

    init
    init

---
+++ 2EzUtility/2EzUtility/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using SharpDX;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+
+namespace _2EzUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Chat.Print("Hello World");
+        }
+    }
+}
